9a06d1a241f77986472c5da6ef1982d2380ef8ad
Ankit Sharma <sankit72@gmail.com>
2024-07-12 22:26:05 +0530

    add commnets for syntax grammar

--- src/main/java/io/github/darkaster/lox/Parser.java
+++ src/main/java/io/github/darkaster/lox/Parser.java
@@ -20,10 +20,14 @@ class Parser {
         }
     }
 
+    // syntax grammar
+    // expression → equality ;
     private Expr expression() {
         return equality();
     }
 
+    // syntax grammar
+    // equality  → comparison ( ( "!=" | "==" ) comparison )* ;
     private Expr equality() {
         Expr expr = comparison();
 
@@ -34,6 +38,83 @@ class Parser {
         }
 
         return expr;
+    }
+
+
+    // syntax grammar
+    //    comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    // syntax grammar
+    // term → factor ( ( "-" | "+" ) factor )* ;
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(PLUS, MINUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+
+    // syntax grammar
+    // factor → unary ( ( "/" | "*" ) unary )* ;
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+
+    // syntax grammar
+    // unary → ( "!" | "-" ) unary | primary ;
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    // syntax grammar
+    // primary  → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(STRING, NUMBER)) {
+            return new Expr.Literal(previous().literal);
+        }
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Expect expression.");
     }
 
     private Token previous() {
@@ -67,70 +148,6 @@ class Parser {
 
     private boolean isAtEnd() {
         return peek().type == EOF;
-    }
-
-    private Expr comparison() {
-        Expr expr = term();
-
-        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
-            Token operator = previous();
-            Expr right = term();
-            expr = new Expr.Binary(expr, operator, right);
-        }
-
-        return expr;
-    }
-
-    private Expr term() {
-        Expr expr = factor();
-
-        while (match(PLUS, MINUS)) {
-            Token operator = previous();
-            Expr right = factor();
-            expr = new Expr.Binary(expr, operator, right);
-        }
-
-        return expr;
-    }
-
-    private Expr factor() {
-        Expr expr = unary();
-
-        while (match(SLASH, STAR)) {
-            Token operator = previous();
-            Expr right = unary();
-            expr = new Expr.Binary(expr, operator, right);
-        }
-
-        return expr;
-    }
-
-    private Expr unary() {
-        if (match(BANG, MINUS)) {
-            Token operator = previous();
-            Expr right = unary();
-
-            return new Expr.Unary(operator, right);
-        }
-
-        return primary();
-    }
-
-    private Expr primary() {
-        if (match(FALSE)) return new Expr.Literal(false);
-        if (match(TRUE)) return new Expr.Literal(true);
-        if (match(NIL)) return new Expr.Literal(null);
-
-        if (match(STRING, NUMBER)) {
-            return new Expr.Literal(previous().literal);
-        }
-        if (match(LEFT_PAREN)) {
-            Expr expr = expression();
-            consume(RIGHT_PAREN, "Expect ')' after expression.");
-            return new Expr.Grouping(expr);
-        }
-
-        throw error(peek(), "Expect expression.");
     }
 
     private Token consume(TokenType tokenType, String message) {
